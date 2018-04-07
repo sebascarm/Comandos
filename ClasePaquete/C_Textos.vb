a67e1aa5096582c3f927287e146468ad0b54b8c1
sebascarm <sebascarm@gmail.com>
2018-04-07 17:16:53 -0300

    Clase Paquete

--- ClasePaquete/C_Textos.vb
+++ ClasePaquete/C_Textos.vb
@@ -1,11 +1,12 @@
 ﻿Public Class C_Textos
     Public Shared MensajeError As String
-    Public Shared Function ExtraerNumero(Paquete As String, Inicio As String, Final As String, Optional PosInicio As Integer = -1, Optional PosFin As Integer = -1) As Integer
+    Public Shared Function ExtraerNumero(Paquete As String, Inicio As String, Final As String, Optional PosInicio As Integer = 0, Optional PosFin As Integer = 0) As Integer
         Dim ValorTexto As String
         Dim Resultado As Integer = -1
+        Dim intTMP As Integer
         ValorTexto = ExtraerTexto(Paquete, Inicio, Final, PosInicio, PosFin)
         If ValorTexto <> "" Then
-            If IsNumeric(ValorTexto) Then
+            If Integer.TryParse(ValorTexto, intTMP) = True Then
                 Resultado = CInt(ValorTexto)
                 Return Resultado
             Else
@@ -16,13 +17,13 @@
         Return Resultado
     End Function
     'CLASE SHARED - NO REQUIERE INSTANCIAR PARA USAR LOS METODOS
-    Public Shared Function ExtraerTexto(Paquete As String, Inicio As String, Final As String, Optional PosInicio As Integer = -1, Optional PosFin As Integer = -1) As String
+    Public Shared Function ExtraerTexto(Paquete As String, Inicio As String, Final As String, Optional PosInicio As Integer = 0, Optional PosFin As Integer = 0) As String
         Dim Ini As Integer
         Dim Fin As Integer
         Dim IniTexto As Integer
         Dim LongTexto As Integer
         Dim Resultado As String = ""
-        Ini = Paquete.IndexOf(Inicio) 'obtenemos el inicio que debe ser 0 // -1 es no encontrado, otro valor seria desfazado
+        Ini = Paquete.IndexOf(Inicio, PosInicio) 'obtenemos el inicio que debe ser 0 // -1 es no encontrado, otro valor seria desfazado
         If Ini > -1 Then 'inicio encontrado
             If (PosInicio > -1) And (PosInicio <> Ini) Then 'inicio especifico declarado no encontrado
                 MensajeError = "Inicio desfazado"
@@ -33,7 +34,7 @@
             'inicio encontrado, pos inicial no declarada o en la posicion especificada. buscamos el final
             Fin = Paquete.IndexOf(Final, Ini + 1) 'revisar cuando el final aparece antes del incio
             If Fin > -1 Then 'final encontrado
-                If (PosFin > -1) And (PosFin <> Fin) Then 'fin especifico declarado no encontrado
+                If (PosFin > 0) And (PosFin <> Fin) Then 'fin especifico declarado no encontrado
                     MensajeError = "Final desfazado"
                     Resultado = ""
                     Return Resultado
@@ -57,6 +58,21 @@
             Return Resultado
         End If
     End Function
-
+    Public Shared Function NumeroaTexto(Numero As Integer, Digitos As Integer) As String
+        Dim Resultado As String
+        Dim Valor As String
+        Dim LongNumero As Integer
+        Dim DigPendiente As Integer
+        Dim Relleno As String = ""
+        Dim I As Integer
+        Valor = CStr(Numero)
+        LongNumero = Valor.Length
+        DigPendiente = Digitos - LongNumero
+        For I = 1 To DigPendiente
+            Relleno = Relleno + "0"
+        Next
+        Resultado = Relleno & Valor
+        Return Resultado
+    End Function
 
 End Class
--- Inicio.vb
+++ Inicio.vb
@@ -1,16 +1,30 @@
 ﻿'Imports ClaseSocket
+Imports ClasePaquete
 Module Inicio
     Public C_PROCESO As C_Procesos = New C_Procesos
-    Public C_PAQUETE As C_PaqueteDatos = New C_PaqueteDatos
+    Public C_PAQUETE As C_Paquete = New C_Paquete
     'Public C_SOCKET As C_Sockets = New C_Sockets
     Public Sub Main()
 
         C_PROCESO.CargarArray()
         C_PROCESO.CargarGrid(FormComandos.GridProc)
         'C_Ejecutar.Ejecutar("calc.exe")
-        Dim seba = C_Ejecutar.EjecutarResul("DaIR")
-        MsgBox(seba)
+        'Dim seba = C_Ejecutar.EjecutarResul("DaIR")
+        'MsgBox(seba)
 
-        'C_PAQUETE.Control(">(001)[01](COMANDO)EXECUTE CALC.EXE<")
+        Dim Paquete As String
+        'Paquete = (">(036)[00](COMANDO)EXECUTE CALC.EXE<")
+
+        Paquete = C_PAQUETE.ArmarPaquete("COMANDO", "CALC.EXE")
+        Debug.Print(Paquete)
+
+
+        If C_PAQUETE.Cargar(Paquete) = True Then
+
+            Debug.Print(C_PAQUETE.Est_Paquete.Longitud)
+            Debug.Print(C_PAQUETE.Est_Paquete.Secuencia)
+            Debug.Print(C_PAQUETE.Est_Paquete.TipoDato)
+            Debug.Print(C_PAQUETE.Est_Paquete.PaqueteDatos)
+        End If
     End Sub
 End Module
