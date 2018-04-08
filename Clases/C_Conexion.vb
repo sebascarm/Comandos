425f9ed7cb2cdf5a2294a3f2a8a846e3add2ecc9
Sebastian Carmona <sebascarm@gmail.com>
2018-04-07 21:07:37 -0300

    Parte de la clase conexion

--- Clases/C_Conexion.vb
+++ Clases/C_Conexion.vb
@@ -1,13 +1,31 @@
 ﻿Imports ClaseSocket
+Imports ClasePaquete
 Public Class C_Conexion
-    Dim C_SOCKET As C_Sockets = New C_Sockets
+    Dim Transmision As C_Sockets
+    Dim Paquete As C_Paquete
+    Dim Estado As Integer = 0
+    Public Sub New()
+
+    End Sub
     Public Sub Escuchar()
         Dim IP As String = FormComandos.TeIP.Text
         Dim Port As Integer = CInt(FormComandos.TePuerto.Text)
-
-        C_SOCKET.Escuchar(IP, Port, 100, 100, "<")
+        'revisar si ya esta escuchando
+        Transmision = New C_Sockets
+        Paquete = New C_Paquete
+        AddHandler Transmision.E_Estado, AddressOf CambioEstado
+        AddHandler Transmision.E_Recepcion, AddressOf Recepcion
+        Transmision.Escuchar(IP, Port, 100, 100, "<")
 
     End Sub
+    Private Sub CambioEstado(ByVal Estado As Integer)
+        Me.Estado = Estado
+    End Sub
+    Private Sub Recepcion(ByVal Datos As String)
+        If Paquete.Cargar(Datos) Then
 
-
+        Else
+            Debug.Print(Paquete.Est_Paquete.MensajeError)
+        End If
+    End Sub
 End Class
